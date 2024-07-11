608cb6cb71809cedcea232e7d7949ef887dff99a
ZavAhmed <2022cs614@student.uet.edu.pk>
2024-07-12 00:34:48 +0500

    Content issue

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -16,47 +16,48 @@ const Banner = () => {
         } catch (error) {
             console.error(`Error fetching data: ${error.message}`);
         }
-
     };
-
 
     useEffect(() => {
         fetchData();
-
     }, []);
 
     const handleSlideChange = (id) => {
-        console.log(`id is: ${id}`)
-    }
-
+        const newMovies = movies.map((movie) => {
+            movie.active = false;
+            if (movie._id === id) {
+                movie.active = true
+            }
+            return movie
+        });
+        setMovies(newMovies); 
+    };
 
     return (
         <div className='banner'>
-            {
-                movies && movies.length > 0 && movies.map(movie => {
-                    return (
-                        <div className="movie">
-                            <img src={movie.bgImg}
-                                alt="Background Image"
-                                className={`bgImg ${movie.active ? 'active' : undefined}`}
-                            />
-                            <div className="container-fluid d-flex justify-content-center">
-                                <div className="row">
-                                    <div className="col-lg-6 col-md-12">
-                                        <MovieContent movie={movie} />
-                                    </div>
-                                    <div className="col-lg-6 col-md-12">
-                                        <MovieDate movie={movie} />
-                                        <PlayBtn movie={movie} />
-                                    </div>
-                                </div>
+            {movies && movies.length > 0 && movies.map((movie) => (
+                <div key={movie._id} className="movie">
+                    <img
+                        src={movie.bgImg}
+                        alt="Background Image"
+                        className={`bgImg ${movie.active ? 'active' : ''}`}
+                    />
+                    <div className="container-fluid d-flex justify-content-center">
+                        <div className="row">
+                            <div className="col-lg-6 col-md-12">
+                                <MovieContent movie={movie} />
+                            </div>
+                            <div className="col-lg-6 col-md-12">
+                                <MovieDate movie={movie} />
+                                <PlayBtn movie={movie} />
                             </div>
                         </div>
-                    )
-
-                })
-            }
-            {movies && movies.length > 0 && <MovieSwiper slides={movies} slideChange={handleSlideChange} />}
+                    </div>
+                </div>
+            ))}
+            {movies && movies.length > 0 && (
+                <MovieSwiper slides={movies} slideChange={handleSlideChange} />
+            )}
         </div>
     );
 };
