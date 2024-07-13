ac4132a53986168288833216875e3a8c2125bffd
ZavAhmed <2022cs614@student.uet.edu.pk>
2024-07-14 00:02:19 +0500

    Responsiveness remaining

--- src/components/NavListItem.jsx
+++ src/components/NavListItem.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './navListItem.css'
 
-const NavListItem = ({ nav }) => {
+const NavListItem = ({ nav, navOnClick }) => {
     return (
-        <li><a href={nav.link}>{nav.name}</a></li>
+        <li>
+            <a href={nav.link}
+                className={`${nav.active ? 'active' : undefined}`}
+                onClick={() => { navOnClick(nav._id) }}>
+                {nav.name}
+            </a>
+        </li>
     )
 }
 
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavListItem from '../components/NavListItem'
 import navListData from '../data/navListData'
 import './header.css'
@@ -6,18 +6,30 @@ import Search from '../components/Search'
 import { SlLogin } from "react-icons/sl";
 import Button from '../components/Button'
 
-const Header = () => {
+const Header = ({ scroll }) => {
+
+    const [navList, setNavList] = useState(navListData);
+
+    const handleNavClick = id => {
+        const newNavList = navList.map(nav => {
+            nav.active = false;
+            if (nav._id === id) nav.active = true;
+            return nav
+        })
+        setNavList(newNavList);
+    }
+
     return (
-        <header>
+        <header className={`${scroll > 100 ? 'scrolled' : undefined}`}>
             <a href="#" className='logo'>ByteBox</a>
             <ul className="nav">
 
-                {navListData.map(nav => (
-                    <NavListItem key={nav._id} nav={nav} />
+                {navList.map(nav => (
+                    <NavListItem key={nav._id} nav={nav} navOnClick={handleNavClick} />
                 ))}
             </ul>
             <Search />
-            <Button icon={<SlLogin />} name="Sign in"/>
+            <Button icon={<SlLogin />} name="Sign in" />
         </header>
     )
 }
